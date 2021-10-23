599b4b5333953b7c7c245cd86ec4afb664c668fd
Janiek Buysrogge <janiek.buysrogge@gmail.com>
2021-10-24 00:14:02 +0200

    initial checkin

---
+++ asset-scanner/asset-scanner/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using blockfrost.Api;
+using blockfrost.Client;
+using blockfrost.Model;
+using Newtonsoft.Json;
+
+namespace asset_scanner
+{
+    class Program
+    {
+        private static CardanoAssetsApi _assetsApi;
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Setup");
+
+            var apiKey = new Dictionary<string, string>();
+            apiKey.Add("project_id", "<REDACTED>");
+            _assetsApi = new CardanoAssetsApi(new Configuration { ApiKey = apiKey });
+
+            Console.WriteLine("Start");
+
+            // Get all assets of a policy
+            var assets = await GetAssetsForPolicy("c805f32096ccb75be6b9d155ba8250b6a23ac9dffd95948551f57eeb");
+            Console.WriteLine($"Number of assets: {assets?.Count ?? 0}");
+
+            // Get details of first asset
+            var asset = await GetAsset(assets.First());
+            Console.WriteLine(JsonConvert.SerializeObject(asset, Formatting.Indented));
+
+            Console.WriteLine(asset.OnchainMetadata.traits.ears);
+
+            Console.WriteLine("done");
+            Console.ReadLine();
+        }
+
+        private static async Task<Asset> GetAsset(AssetPolicyInner asset)
+        {
+            return await _assetsApi.AssetsAssetGetAsync(asset.Asset);
+        }
+
+        private static async Task<List<AssetPolicyInner>> GetAssetsForPolicy(string policyId)
+        {
+            var assets = new List<AssetPolicyInner>();
+
+            int page = 1;
+            while (true)
+            {
+                var pagedAssets = await _assetsApi.AssetsPolicyPolicyIdGetAsync(policyId, count: 100, page: page);
+
+                if (pagedAssets.Count == 0)
+                {
+                    break;
+                }
+                
+                assets.AddRange(pagedAssets);
+
+                page++;
+            };            
+
+            return assets;
+        }
+    }
+}
---
+++ asset-scanner/blockfrost-sdk/Model/AssetOnchainMetadata.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+
+namespace blockfrost.Model
+{
+    public partial class AssetOnchainMetadata___
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("additionalProperties")]
+        public bool AdditionalProperties { get; set; }
+
+        [JsonProperty("nullable")]
+        public bool Nullable { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("properties")]
+        public Properties Properties { get; set; }
+    }
+
+    public partial class Properties
+    {
+        [JsonProperty("name")]
+        public Name Name { get; set; }
+
+        [JsonProperty("image")]
+        public Image Image { get; set; }
+    }
+
+    public partial class Image
+    {
+        [JsonProperty("oneOf")]
+        public OneOf[] OneOf { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("example")]
+        public string Example { get; set; }
+    }
+
+    public partial class OneOf
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+
+    public partial class Name
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("example")]
+        public string Example { get; set; }
+    }
+}
